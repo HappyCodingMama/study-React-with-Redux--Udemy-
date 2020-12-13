e34c86a5b124a19b4b75d1fa62ca74eb095057c6
HappyCodingMama <67956944+HappyCodingMama@users.noreply.github.com>
2020-12-13 16:43:02 +0900

    communicating with props

--- project001/components/src/index.js
+++ project001/components/src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jane" />
+            <ApprovalCard>
+                Are you sure you want to do this?
+            </ApprovalCard>
+
+
+            <ApprovalCard>
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" avatar={faker.image.image()} />
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail author="Alex" timeAgo="Today at 6:24PM" content="I like the subject" avatar={faker.image.image()} />
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail author="Jane" timeAgo="Today at 3:13PM" content="Good!" avatar={faker.image.image()} />
+            </ApprovalCard>
         </div>
     );
 };
