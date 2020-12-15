fdb20a8c87f051b63fe7e7036394d209a4ddcd08
HappyCodingMama <67956944+HappyCodingMama@users.noreply.github.com>
2020-12-16 00:49:28 +0900

    build the simple search app

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+      summer: {
+            text: 'Let\'s hit the beach!',
+            iconName: 'sun'
+      },
+      winter: {
+            text: 'Burr it is cold!',
+            iconName: 'snowflake'
+      }
+};
 
 const getSeason = (lat, month) => {
 if (month > 2 && month < 9) {
@@ -10,10 +22,13 @@ if (month > 2 && month < 9) {
 
 const  SeasonDisplay= props => {
       const season = getSeason(props.lat, new Date().getMonth());
-      console.log(season);
+     const {text, iconName } = seasonConfig[season]; // { text,iconName }
+    
       return (
-            <div>
-              Season Display    
+            <div className={`season-display ${season}`}>
+                  <i className={`icon-left massive ${iconName} icon`} />
+             <h1>{text}</h1>
+             <i className={`icon-right massive ${iconName} icon`}  />
             </div>
       )
 }
