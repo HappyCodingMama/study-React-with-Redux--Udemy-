61c2c1d95fb8b490f8c62808a2939eb82a39fdbe
HappyCodingMama <67956944+HappyCodingMama@users.noreply.github.com>
2020-12-26 14:46:36 +0900

    finished the blog App

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,3 +1,13 @@
-export default () => {
- return 123;
+const postsReducer = (state=[], action) => {
+ switch (action.type) {
+    case 'FETCH_POSTS':
+         return action.payload;
+    case 'ADD_POST':
+         return [...state, action.payload];     
+    default:
+         return state;
+ }
 };
+
+export default postsReducer;
+
